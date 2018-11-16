f7ef51ab07e6d3f348cd5fcfe7febceef40ea8b0
Samir Omarov <samir.omarov@gmail.com>
2018-11-16 03:28:36 +0300

    Add files via upload

---
+++ 9.4/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Time.h"
+#include <ctime>
+
+int main()
+{
+	Time time1;
+	Time time2(23, 59, 58);
+
+	std::cout << "time1: ";
+	time1.print();
+
+	std::cout << "\n\ntime2: ";
+	time2.print();
+	std::cout << "\n\ntick function (seconds increments by 1 but show by 10) : ";
+	for (int i = 0; i < 100; ++i) {
+		time2.tick();
+
+		if (i % 10 == 0) {
+			time2.print();
+
+			std::cout << " ";
+
+			std::cout << std::endl;
+		}
+	}
+
+	system("pause"); 
+}
---
+++ 9.4/Time.cpp
@@ -0,0 +1,92 @@
+#include "Time.h"
+
+#include <ctime>
+
+#include <iostream>
+
+
+Time::Time() {
+	// Initialize to the current time
+	time_t currenttime;
+	struct tm timeinfo;
+
+	time(&currenttime);
+	localtime_s(&timeinfo, &currenttime); //convert to local time
+
+	hour = timeinfo.tm_hour;
+	minute = timeinfo.tm_min;
+	second = timeinfo.tm_sec;
+}
+Time::Time(int hour, int minute, int second) { setTime(hour, minute, second); }
+// SET
+// set new Time value
+void Time::setTime(int h, int m, int s) {
+	setHour(h);
+	setMinute(m);
+	setSecond(s);
+}
+// set hours
+void Time::setHour(int h) {
+	if (h >= 0 && h < 24) {
+		hour = h;
+	}
+	else {
+		std::cout << "hour must be 0-23" << std::endl;
+	}
+}
+// set minutes
+void Time::setMinute(int m) {
+	if (m >= 0 && m < 60) {
+		minute = m;
+	}
+	else {
+		std::cout << "minute must be 0-59" << std::endl;
+	}
+}
+// set seconds
+void Time::setSecond(int s) {
+	if (s >= 0 && s < 60) {
+		second = s;
+	}
+	else {
+		std::cout << "seconds must be 0-59" << std::endl;
+	}
+}
+// GET
+int Time::getHour() { return hour; }
+int Time::getMinute() { return minute; }
+int Time::getSecond() { return second; }
+
+
+void Time::tick() {
+	if (second < 59) {
+		second++;
+	}
+	else {
+		second = 0;
+		if (minute < 59) {
+			minute++;
+		}
+		else {
+			minute = 0;
+			if (hour < 23) {
+				hour++;
+			}
+			else {
+				hour = 0;
+				minute = 0;
+				second = 0;
+			}
+		}
+	}
+}
+
+
+void Time::print() {
+	std::cout << hour << ":" << minute << ":" << second << std::endl;
+}
+
+
+
+
+
